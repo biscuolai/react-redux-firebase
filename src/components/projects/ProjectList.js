acf123524c0a0709cec381b31b1c46c46c267d2a
biscuolai <ilson_biscuola@yahoo.com>
2018-09-20 20:27:46 +0800

    project reducer

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
-import ProjectSummary from './ProjectSummary';
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
-export default class ProjectList extends Component {
-    render() {
+const ProjectList = ({projects}) => {
+  return (
+    <div className="project-list section">
+      { projects && projects.map(project => {
         return (
-            <div className='project-list section'>
-                <ProjectSummary />
-                <ProjectSummary />
-                <ProjectSummary />
-            </div>
+          <ProjectSummary project={project} key={project.id} />
         )
-    }
+      })}  
+    </div>
+  )
 }
+
+export default ProjectList
