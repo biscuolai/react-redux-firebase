1f9868ebc116bd40ebf64ebafd76853c0a94af4d
biscuolai <ilson_biscuola@yahoo.com>
2018-09-20 21:39:08 +0800

    create project action and reducer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+  );
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
     <Provider store={store}>
