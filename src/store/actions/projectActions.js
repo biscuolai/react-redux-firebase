ef8392c7609cd3b79c20dc16c2d2ed0634f4600a
biscuolai <ilson_biscuola@yahoo.com>
2018-09-22 19:03:19 +0800

    project name

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,12 +1,15 @@
 export const createProject = (project) => {
   return (dispatch, getState, {getFirestore}) => {
+
+    // console.log('getstate', getState().firebase.auth.uid);
+
     // make async call to database
     const firestore = getFirestore();
     firestore.collection('projects').add({
       ...project,
       authorFirstName: 'Ilson',
       authorLastName: 'Biscuola',
-      authorId: 1,
+      authorId: '1',
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
