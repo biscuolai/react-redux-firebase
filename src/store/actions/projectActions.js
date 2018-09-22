32eb6273ca3068ebd689372021ec44905f89afdb
biscuolai <ilson_biscuola@yahoo.com>
2018-09-22 18:42:42 +0800

    create project working

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,9 +4,9 @@ export const createProject = (project) => {
     const firestore = getFirestore();
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
+      authorFirstName: 'Ilson',
+      authorLastName: 'Biscuola',
+      authorId: 1,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
